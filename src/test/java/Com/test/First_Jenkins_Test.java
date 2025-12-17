24ec7ee009af8912ab557b9af3ab4d60e9649ec1
Akshay Dhengre <akshaydhengre430@gmail.com>
2025-12-17 18:11:29 +0530

    Initial commit

---
+++ src/test/java/Com/test/First_Jenkins_Test.java
@@ -0,0 +1,18 @@
+package Com.test;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class First_Jenkins_Test {
+
+    // method under test
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    @Test
+    public void testAdd() {
+        First_Jenkins_Test calc = new First_Jenkins_Test();
+        assertEquals(6, calc.add(3, 3));
+    }
+}
