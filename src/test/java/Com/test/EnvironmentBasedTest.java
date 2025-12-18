3bb6141ea86002cf492c06fc53c26e6a01b24116
Akshay Dhengre <akshaydhengre430@gmail.com>
2025-12-18 12:31:58 +0530

    new

--- src/test/java/Com/test/EnvironmentBasedTest.java
+++ src/test/java/Com/test/EnvironmentBasedTest.java
@@ -13,7 +13,7 @@ public class EnvironmentBasedTest {
     @Test
     public void testAdd() {
         First_Jenkins_Test calc = new First_Jenkins_Test();
-        assertEquals(20, calc.add(10, 10));
+        assertEquals(20, calc.add(10, 102));
         
        
       
