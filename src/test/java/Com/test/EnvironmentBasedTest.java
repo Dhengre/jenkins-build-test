5cc24259e12ed5dac15b16c72f8291083c6e62b1
Akshay Dhengre <akshaydhengre430@gmail.com>
2025-12-18 12:17:10 +0530

    MAIN

--- src/test/java/Com/test/EnvironmentBasedTest.java
+++ src/test/java/Com/test/EnvironmentBasedTest.java
@@ -5,6 +5,11 @@ import org.junit.Test;
 
 public class EnvironmentBasedTest {
 
+	   // method under test
+    public int add(int a, int b) {
+        return a + b;
+    }
+    
     @Test
     public void testAdd() {
         First_Jenkins_Test calc = new First_Jenkins_Test();
