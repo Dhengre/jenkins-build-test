a354946bf0bbb3b0aeb6ab9cb236aaac1d960a11
Akshay Dhengre <akshaydhengre430@gmail.com>
2025-12-18 18:20:50 +0530

    test main

--- src/test/java/Com/test/EnvironmentBasedTest.java
+++ src/test/java/Com/test/EnvironmentBasedTest.java
@@ -13,7 +13,7 @@ public class EnvironmentBasedTest {
     @Test
     public void testAdd() {
         First_Jenkins_Test calc = new First_Jenkins_Test();
-        assertEquals(1600, calc.add(800, 800));
+        assertEquals(1600, calc.add(800, 700));
         
       
     }
